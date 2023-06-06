520c2f5459901e6eb6fad09f2fdc55c54792cd3a
Ed Paradis <eparadis@users.noreply.github.com>
2023-06-05 21:07:01 -0700

    reformat

--- nmi.c
+++ nmi.c
@@ -8,21 +8,22 @@ extern unsigned int g_nmi;
 /* Implementation for the NMI device */
 void nmi_device_reset(void)
 {
-	g_nmi = 0;
+  g_nmi = 0;
 }
 
 void nmi_device_update(void)
 {
-	if(g_nmi)
-	{
-		g_nmi = 0;
-		int_controller_set(IRQ_NMI);
-	}
+  if (g_nmi)
+  {
+    g_nmi = 0;
+    int_controller_set(IRQ_NMI);
+  }
 }
 
 int nmi_device_ack(void)
 {
-	printf("\nNMI\n");fflush(stdout);
-	int_controller_clear(IRQ_NMI);
-	return M68K_INT_ACK_AUTOVECTOR;
+  printf("\nNMI\n");
+  fflush(stdout);
+  int_controller_clear(IRQ_NMI);
+  return M68K_INT_ACK_AUTOVECTOR;
 }
