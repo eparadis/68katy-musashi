7f64ad9c6804f404e410caf2ba06add288ec4672
Ed Paradis <eparadis@users.noreply.github.com>
2023-06-05 22:13:06 -0700

    include error

--- sim.h
+++ sim.h
@@ -1,6 +1,10 @@
 #ifndef SIM__HEADER
 #define SIM__HEADER
 
+// some of the musashi stuff uses 'sim.h'
+// so include the definition of our IRQ handler function here
+#include "irqs.h"
+
 void cpu_pulse_reset(void);
 void cpu_set_fc(unsigned int fc);
 void cpu_instr_callback(int pc);
@@ -9,10 +13,6 @@ void exit_error(char* fmt, ...);
 
 void cpu_pulse_reset(void);
 void cpu_set_fc(unsigned int fc);
-int cpu_irq_ack(int level);
-
-int timer_device_ack(void);
-void timer_update(void);
 
 void get_user_input(void);
 
